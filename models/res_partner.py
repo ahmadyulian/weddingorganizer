a34c8d66dc0da04e75639a93731e03e7e882a9ae
Ahmad Yulian Dinata <Ahmad.Yulian.Dinata@hashmicro.com>
2022-03-15 17:32:09 +0700

    Change rest_partner.py and pegawai_views.xml

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,11 +1,9 @@
 from odoo import api, fields,models
 
-
 class Partner(models.Model):
+    #inherit dengan modul res.partner
     _inherit = 'res.partner'
     
+    #ofbool
     is_pegawainya = fields.Boolean(string='Pegawai', default=False)
     is_customernya = fields.Boolean(string='Customer', default=False)
-    
-    
-    
