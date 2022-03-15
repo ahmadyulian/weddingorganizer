d2feaf7e71fac155ec50eec2dc12245937e50896
Ahmad Yulian Dinata <Ahmad.Yulian.Dinata@hashmicro.com>
2022-03-15 17:29:26 +0700

    Fix Pelaminan

--- models/pelaminan.py
+++ models/pelaminan.py
@@ -1,11 +1,12 @@
 from odoo import api, fields, models
 
-
 class Pelaminan(models.Model):
     _name = 'wedding.pelaminan'
     _description = 'Daftar Tip'
 
+    #ofchar
     name = fields.Char(string='Name')
     deskripsi = fields.Char(string='Deskripsi Pelaminan')
+
+    #ofint
     harga = fields.Integer(string='Harga Sewa')
-    
