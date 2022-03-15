15b56aa05980e526d4aa126749bbed2bea98ba55
Ahmad Yulian Dinata <Ahmad.Yulian.Dinata@hashmicro.com>
2022-03-15 17:47:13 +0700

    Modified Kursi Pengantin

--- models/kursipengantin.py
+++ models/kursipengantin.py
@@ -1,12 +1,12 @@
 from odoo import api, fields, models
 
-
 class KursiPengantin(models.Model):
     _name = 'wedding.kursipengantin'
     _description = 'Daftar Kursi Pengantin Pelaminan'
 
+    #ofchar
     name = fields.Char(string='Name')
     deskripsi = fields.Char(string='Deskripsi Kursi Pengantin')
+
+    #ofint
     harga = fields.Integer(string='harga sewa')
-    
-    
