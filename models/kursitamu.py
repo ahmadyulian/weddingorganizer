d6f408d65970e668c5d50f2c6caa1d25646173a0
Ahmad Yulian Dinata <Ahmad.Yulian.Dinata@hashmicro.com>
2022-03-15 17:45:55 +0700

    Change and Modified Kursi Tamu

--- models/kursitamu.py
+++ models/kursitamu.py
@@ -1,14 +1,16 @@
 from odoo import api, fields, models
 
-
 class KursiTamu(models.Model):
     _name = 'wedding.kursitamu'
     _description = 'Data tentang kursi tamu dan harganya'
 
+    #ofchar
     name = fields.Char(string='Name')
-    tipe = fields.Selection(string='Tipe Kursi', selection=[('plastik','Plastik'), ('stainless','Stainless')])
+    
+    #ofsel
+    tipe = fields.Selection(string='Tipe Kursi', 
+    selection=[('plastik','Plastik'), ('stainless','Stainless')])
+    
+    #ofint
     stok = fields.Integer(string='Stok Kursi')
     harga = fields.Integer(string='Harga Sewa per Unit')
-    
-    
-    
