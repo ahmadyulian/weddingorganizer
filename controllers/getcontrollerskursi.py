1aa071806390a7958ca9b7e36be03cca290e676d
Ahmad Yulian Dinata <Ahmad.Yulian.Dinata@hashmicro.com>
2022-03-23 10:34:14 +0700

    Modified getcontrollerkursi.py

--- controllers/getcontrollerskursi.py
+++ controllers/getcontrollerskursi.py
@@ -13,3 +13,26 @@ class KursiTamuController(http.Controller):
                          "stok_tersedia" : k.stok,
                          "harga_sewa" : k.harga})
         return json.dumps(value)
+    else:
+            kursiid = request.env['wedding.kursitamu'].search([('id','=',idnya)])
+            for k in kursiid:
+                value.append({"id": k.id,
+                            "namakursi" : k.name,
+                            "tipe_bahan" : k.tipe,
+                            "stok_tersedia" : k.stok,
+                            "harga_sewa" : k.harga})
+            return json.dumps(value)
+    
+    @http.route('/createkursi',auth='user', type='json', methods=['POST'])
+    def createKursi(self, **kw):    
+        if request.jsonrequest:    
+            if kw['name']:
+                vals={
+                    'name': kw['name'], 
+                    'tipe' : kw['tipe'],
+                    'stok' : kw['stok'],
+                    'harga' : kw['harga'],
+                }
+                kursibaru = request.env['wedding.kursitamu'].create(vals)
+                args = {'success': True, 'ID':kursibaru.id}
+                return args
